11ca0752b023966ba77eb43b4b2bc1f1de918874
johanaluna <johanaluna@users.noreply.github.com>
2020-03-04 14:10:13 -0500

    updates

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -122,9 +122,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny'
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    # ],
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
